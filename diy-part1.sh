64397e5e7fcdda94f6792d78924ea9d64c5cddc4
AL <alex.open@gmail.com>
2026-08-20 12:58:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git adguardhome https://github.com/kuoruan/openwrt-upx.git' >> feeds.conf.default
+echo 'src-git adguardhome_app https://github.com/kuoruan/luci-app-adguardhome.git' >> feeds.conf.default
